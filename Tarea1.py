a3d6c3ff8179eb7992c614ca4b96ddaf0cae8b47
Enzo <enzo.lpez.pstor@gmail.com>
2024-04-22 00:48:31 -0400

    entrega tarea1

---
+++ Tarea1.py
@@ -0,0 +1,159 @@
+
+import trimesh as tm
+import os
+import sys
+from pathlib import Path
+import numpy as np
+import pyglet
+import pyglet.gl as GL
+import trimesh as tm
+from pyglet.window import key
+
+#Import clases propias
+import aux_functions.transformations as tr
+from classes.controller import *
+from classes.models import *
+
+PERSPECTIVE_VIEW = 0
+ORTOGRAPHIC_VIEW = 1
+
+
+if __name__ == "__main__":
+
+    window = pyglet.window.Window(960, 960)
+
+    controller = Controller()
+
+    @window.event
+    def on_key_press(symbol, modifier):
+        # print(symbol)
+        if(key.C == symbol):
+            controller.change_view()
+        if(key.SPACE == symbol):
+            controller.change_view()
+
+    #Usamos la clase objeto para crear los objetos a graficar
+    #Generamos el tablero
+    tablero = Object_Static("superficie.obj",2.0)
+
+    #Generamos las paredes
+    paredes = Object_Static("paredes.obj",2.0)
+
+    #Generamos los obstaculos en el tablero
+    #Creamos el Primer Bumper
+    bumper_1 = Object_Static("bumper.obj",1.0)
+
+    #Segundo Bumper
+    bumper_2 = Object_Static("bumper.obj",1.0)
+
+    #Creamos el primer flipper flipper_1
+    flipper_1 = Flipper()
+
+    #creamnos el segundo flipper flipper_2
+    flipper_2 = Flipper()
+
+    # GAME LOOP
+    @window.event
+    def on_draw():
+        GL.glClearColor(0, 0, 0, 1.0)
+        window.clear()
+        
+        #Definimos perspectiva y camara de vista
+        if(controller.view == PERSPECTIVE_VIEW):
+            projection = tr.perspective(45, window.width/window.height, 0.001, 100).reshape(16, 1, order='F')
+            camera_view = tr.lookAt(
+                np.array([3,3,3]),
+                np.array([0, 0, 0]), 
+                np.array([0, 0, 1])
+            ).reshape(16, 1, order='F')
+        if(controller.view == ORTOGRAPHIC_VIEW): 
+            camera_view = tr.lookAt(
+                np.array([3,3,3]),
+                np.array([0, 0, 0]), 
+                np.array([0, 0, 1])
+            ).reshape(16, 1, order='F')
+            projection = tr.ortho(-2, 2, -2, 2, 0.001, 10.0).reshape(16, 1, order='F')
+
+        #Dibujamos el tablero
+        GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_FILL)
+        tablero_translate = tr.translate(0, 0, 0)
+        tablero_rotation = tr.rotationX(np.pi/2) @ tr.rotationY(np.pi/4)
+        tablero_scale = tr.scale(2, 2, 2)
+        tablero_color = [0.5, 0.5, 1]
+        tablero.set_pipeline(projection, camera_view, tablero_translate, tablero_rotation, tablero_scale, tablero_color)
+        tablero_pipeline = tablero.pipeline()
+        tablero_gpu = tablero.gpu()
+        tablero_pipeline.use()
+        tablero_gpu.draw(GL.GL_TRIANGLES)
+
+        #Dibujamos las paredes
+        GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_FILL)
+        paredes_translate = tr.translate(-0.145, 0, -0.08)
+        paredes_rotation = tr.rotationX(np.pi/2) @ tr.rotationY(np.pi/4)
+        paredes_scale = tr.scale(2.1, 2.1, 2.1)
+        paredes_color = [0.5, 0.5, 0.5]
+        paredes.set_pipeline(projection, camera_view, paredes_translate, paredes_rotation, paredes_scale, paredes_color)
+        paredes_pipeline = paredes.pipeline()
+        paredes_gpu = paredes.gpu()
+        paredes_pipeline.use()
+        paredes_gpu.draw(GL.GL_TRIANGLES)
+
+  
+        #Dibujamos los obstaculos
+        #Dibujamos el primer bumper
+        GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_FILL)
+        bumper_1_translate =  tr.translate(-1.5, 0, 1)
+        bumper_1_rotation = tr.rotationX(np.pi/2) @ tr.rotationY(np.pi/4)
+        bumper_1_scale = tr.scale(0.4,0.4,0.4)
+        bumper_1_color = [1, 1, 0.5]
+        bumper_1.set_pipeline(projection, camera_view, bumper_1_translate, bumper_1_rotation, bumper_1_scale, bumper_1_color)
+        bumper_1_pipeline = bumper_1.pipeline()
+        bumper_1_gpu =bumper_1.gpu()
+        bumper_1_pipeline.use()
+        bumper_1_gpu.draw(GL.GL_TRIANGLES)
+
+        #Dibujamos el segundo bumper
+        GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_FILL)
+        bumper_2_translate =  tr.translate(1.5, 0, -0.5)
+        bumper_2_rotation = tr.rotationX(np.pi/2) @ tr.rotationY(np.pi/4)
+        bumper_2_scale = tr.scale(0.4,0.4,0.4)
+        bumper_2_color = [1, 1, 0.5]
+        bumper_2.set_pipeline(projection, camera_view, bumper_2_translate, bumper_2_rotation, bumper_2_scale, bumper_2_color)
+        bumper_2_pipeline = bumper_2.pipeline()
+        bumper_2_gpu =bumper_2.gpu()
+        bumper_2_pipeline.use()
+        bumper_2_gpu.draw(GL.GL_TRIANGLES)
+
+        #Dibujamos el primer flipper
+        GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_FILL)
+        flipper_1_pipeline = flipper_1.pipeline()
+        flipper_1_gpu = flipper_1.gpu()
+        flipper_1_pipeline.use()
+        flipper_1_gpu.draw(GL.GL_TRIANGLES)
+
+        matrix_rotation = tr.rotationX(np.pi/2)
+        flipper_1_pipeline['scale'] = tr.scale(0.4,0.4,0.4).reshape(16, 1, order='F')
+        flipper_1_pipeline['rotation'] = matrix_rotation.reshape(16, 1, order='F')
+        flipper_1_pipeline['translate'] = tr.translate(2.5, 0, -3).reshape(16, 1, order='F')
+
+        flipper_1_pipeline['view'] = camera_view
+        flipper_1_pipeline['projection'] = projection
+        flipper_1_pipeline['color'] = [0.5, 1, 0.5]
+
+        #Segundo flipper
+        GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_FILL)
+        flipper_2_pipeline = flipper_2.pipeline()
+        flipper_2_gpu = flipper_2.gpu()
+        flipper_2_pipeline.use()
+        flipper_2_gpu.draw(GL.GL_TRIANGLES)
+
+        matrix_rotation = tr.rotationX(np.pi/2) @ tr.rotationY(np.pi/1.6)
+        flipper_2_pipeline['scale'] = tr.scale(0.4,0.4,0.4).reshape(16, 1, order='F')
+        flipper_2_pipeline['rotation'] = matrix_rotation.reshape(16, 1, order='F')
+        flipper_2_pipeline['translate'] = tr.translate(0.4,0,4).reshape(16, 1, order='F')
+        flipper_2_pipeline['view'] = camera_view
+        flipper_2_pipeline['projection'] = projection
+        flipper_2_pipeline['color'] = [0.5, 1, 0.5]
+
+    # aquí comienza pyglet a ejecutar su loop.
+    pyglet.app.run()
